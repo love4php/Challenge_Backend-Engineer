3725231e006bbc66a5d5b5d5001fb93a89236eb0
meysam eradeshahi <love4php@yahoo.com>
2022-04-05 15:50:07 +0430

    Redirect to /products

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/', function () {
+    return redirect('/products');
+});
 Route::get('/products', '\App\Http\Controllers\ApiController@index');
